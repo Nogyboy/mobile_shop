36809c433e3be0754179664c5c51b3fc389c0c4a
Nogyboy <guamanc9@gmail.com>
2024-11-18 23:56:58 +0000

    style: add spinner

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -14,7 +14,12 @@ function ProductListPage () {
         <h1 className='text-3xl font-bold'>Product List</h1>
         {/* Search Bar */}
         <label className='input input-bordered flex items-center gap-2'>
-          <input type='text' className='grow' placeholder='Search' onChange={(e) => debouncedSearch(e.target.value)} />
+          <input
+            type='text'
+            className='grow'
+            placeholder='Search'
+            onChange={(e) => debouncedSearch(e.target.value)}
+          />
           <svg
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 16 16'
@@ -35,9 +40,53 @@ function ProductListPage () {
           <ProductListCard key={index} product={product} />
         ))}
         {isLoading && (
-          <div className='flex justify-center items-center'>
-            <div className='animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-200' />
-          </div>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='1em'
+            height='1em'
+            viewBox='0 0 24 24'
+          >
+            <g
+              fill='none'
+              stroke='currentColor'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+            >
+              <path
+                stroke-dasharray='16'
+                stroke-dashoffset='16'
+                d='M12 3c4.97 0 9 4.03 9 9'
+              >
+                <animate
+                  fill='freeze'
+                  attributeName='stroke-dashoffset'
+                  dur='0.3s'
+                  values='16;0'
+                />
+                <animateTransform
+                  attributeName='transform'
+                  dur='1.5s'
+                  repeatCount='indefinite'
+                  type='rotate'
+                  values='0 12 12;360 12 12'
+                />
+              </path>
+              <path
+                stroke-dasharray='64'
+                stroke-dashoffset='64'
+                stroke-opacity='0.3'
+                d='M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z'
+              >
+                <animate
+                  fill='freeze'
+                  attributeName='stroke-dashoffset'
+                  dur='1.2s'
+                  values='64;0'
+                />
+              </path>
+            </g>
+          </svg>
         )}
         {error && (
           <div className='flex justify-center items-center'>
